bfbd59f5820d09fb0d4f21799279c4b6aa52feaa
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-03-02 20:31:20 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,144 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use mpu6050_dmp::sensor::Mpu6050;
+use panic_rtt_target as _;
+use rtic::app;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f1xx_hal::delay::Delay;
+use stm32f1xx_hal::gpio::CRL;
+use stm32f1xx_hal::gpio::{gpioc::PC13, Output, PinState, PushPull};
+use stm32f1xx_hal::gpio::{Alternate, OpenDrain};
+use stm32f1xx_hal::i2c::{BlockingI2c, DutyCycle, Mode};
+use stm32f1xx_hal::pac::I2C1;
+use stm32f1xx_hal::prelude::*;
+use systick_monotonic::{fugit::Duration, Systick};
+
+//mod test_imu;
+
+type Mpu = Mpu6050<
+    stm32f1xx_hal::i2c::BlockingI2c<
+        I2C1,
+        (
+            stm32f1xx_hal::gpio::Pin<Alternate<OpenDrain>, CRL, 'B', 6_u8>,
+            stm32f1xx_hal::gpio::Pin<Alternate<OpenDrain>, CRL, 'B', 7_u8>,
+        ),
+    >,
+>;
+
+#[app(device = stm32f1xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+mod app {
+    use super::*;
+    use mpu6050_dmp::yaw_pitch_roll::YawPitchRoll;
+
+    #[shared]
+    struct Shared {
+        mpu: Mpu,
+    }
+
+    #[local]
+    struct Local {
+        led: PC13<Output<PushPull>>,
+        state: bool,
+    }
+
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1000>;
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // Setup clocks
+        let mut flash = cx.device.FLASH.constrain();
+        let rcc = cx.device.RCC.constrain();
+        let mut afio = cx.device.AFIO.constrain();
+
+        rtt_init_print!();
+        rprintln!("init");
+
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.mhz())
+            .sysclk(36.mhz())
+            .pclk1(36.mhz())
+            .freeze(&mut flash.acr);
+
+        // Setup LED
+        let mut gpioc = cx.device.GPIOC.split();
+        let led = gpioc
+            .pc13
+            .into_push_pull_output_with_state(&mut gpioc.crh, PinState::Low);
+
+        let mut gpiob = cx.device.GPIOB.split();
+        let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+        let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
+
+        // Setup i2c
+        let i2c = BlockingI2c::i2c1(
+            cx.device.I2C1,
+            (scl, sda),
+            &mut afio.mapr,
+            Mode::Fast {
+                frequency: 400_000.hz(),
+                duty_cycle: DutyCycle::Ratio16to9,
+            },
+            clocks,
+            1000,
+            10,
+            1000,
+            1000,
+        );
+
+        let mut delay = Delay::new(cx.core.SYST, clocks);
+
+        let mut sensor =
+            mpu6050_dmp::sensor::Mpu6050::new(i2c, mpu6050_dmp::address::Address::default()).unwrap();
+
+        sensor.initialize_dmp(&mut delay).unwrap();
+
+        let syst = delay.free();
+
+        let mono = Systick::new(syst, 36_000_000);
+
+        // Schedule the blinking task
+        blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+        attitude::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+
+        (
+            Shared { mpu: sensor },
+            Local { led, state: false },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[task(local = [led, state])]
+    fn blink(cx: blink::Context) {
+        rprintln!("blink");
+        if *cx.local.state {
+            cx.local.led.set_high();
+            *cx.local.state = false;
+        } else {
+            cx.local.led.set_low();
+            *cx.local.state = true;
+        }
+        blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
+
+    #[task(shared = [mpu])]
+    fn attitude(mut cx: attitude::Context) {
+        rprintln!("attitude");
+        cx.shared.mpu.lock(|sensor| {
+            // get roll and pitch estimate
+            let len = sensor.get_fifo_count().unwrap();
+            if len >= 28 {
+                let mut buf = [0; 28];
+                let buf = sensor.read_fifo(&mut buf).unwrap();
+                let quat = mpu6050_dmp::quaternion::Quaternion::from_bytes(&buf).unwrap();
+                let ypr = YawPitchRoll::from(quat);
+                rprintln!("{:?}", ypr);
+            }
+        });
+
+        attitude::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
+}
---
+++ src/test_imu.rs
@@ -0,0 +1,81 @@
+use crate::Mpu;
+use mpu6050::device::{AccelRange, GyroRange, ACCEL_HPF, CLKSEL};
+use rtt_target::rprintln;
+use stm32f1xx_hal::delay::Delay;
+
+pub(crate) fn test_imu(mpu: &mut Mpu, delay: &mut Delay) {
+    // Test power management
+    rprintln!("Test power management");
+
+    // Test gyro config
+    rprintln!("Test gyro config");
+    assert_eq!(mpu.get_gyro_range().unwrap(), GyroRange::D250);
+    mpu.set_gyro_range(GyroRange::D500).unwrap();
+    assert_eq!(mpu.get_gyro_range().unwrap(), GyroRange::D500);
+
+    // Test accel config
+    rprintln!("Test accel config");
+    assert_eq!(mpu.get_accel_range().unwrap(), AccelRange::G2);
+    mpu.set_accel_range(AccelRange::G4).unwrap();
+    assert_eq!(mpu.get_accel_range().unwrap(), AccelRange::G4);
+
+    // accel_hpf: per default RESET/no filter, see ACCEL_CONFIG
+    rprintln!("Test accel hpf");
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_RESET);
+    mpu.set_accel_hpf(ACCEL_HPF::_1P25).unwrap();
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_1P25);
+    mpu.set_accel_hpf(ACCEL_HPF::_2P5).unwrap();
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_2P5);
+    mpu.set_accel_hpf(ACCEL_HPF::_5).unwrap();
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_5);
+    mpu.set_accel_hpf(ACCEL_HPF::_0P63).unwrap();
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_0P63);
+    mpu.set_accel_hpf(ACCEL_HPF::_HOLD).unwrap();
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_HOLD);
+
+    // test sleep. Default no, in wake()
+    rprintln!("Test sleep");
+    assert_eq!(mpu.get_sleep_enabled().unwrap(), false);
+    mpu.set_sleep_enabled(true).unwrap();
+    assert_eq!(mpu.get_sleep_enabled().unwrap(), true);
+    mpu.set_sleep_enabled(false).unwrap();
+    assert_eq!(mpu.get_sleep_enabled().unwrap(), false);
+
+    // test temp enable/disable
+    rprintln!("Test temp enable/disable");
+    mpu.set_temp_enabled(false).unwrap();
+    assert_eq!(mpu.get_temp_enabled().unwrap(), false);
+    //assert_eq!(mpu.get_temp().unwrap(), 36.53); // This fails?
+    mpu.set_temp_enabled(true).unwrap();
+    assert_eq!(mpu.get_temp_enabled().unwrap(), true);
+    assert_ne!(mpu.get_temp().unwrap(), 36.53);
+
+    // Test clksel: GXAXIS per default, set in wake()
+    rprintln!("Test CLKSEL");
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::GXAXIS);
+    mpu.set_clock_source(CLKSEL::GYAXIS).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::GYAXIS);
+    mpu.set_clock_source(CLKSEL::GZAXIS).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::GZAXIS);
+    mpu.set_clock_source(CLKSEL::OSCILL).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::OSCILL);
+    mpu.set_clock_source(CLKSEL::STOP).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::STOP);
+    mpu.set_clock_source(CLKSEL::RESERV).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::RESERV);
+    mpu.set_clock_source(CLKSEL::EXT_19P2).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::EXT_19P2);
+    mpu.set_clock_source(CLKSEL::EXT_32p7).unwrap();
+    assert_eq!(mpu.get_clock_source().unwrap(), CLKSEL::EXT_32p7);
+
+    // reset
+    rprintln!("Test reset");
+    mpu.reset_device(delay).unwrap();
+    assert_eq!(mpu.get_accel_hpf().unwrap(), ACCEL_HPF::_RESET);
+    assert_eq!(mpu.get_accel_range().unwrap(), AccelRange::G2);
+    assert_eq!(mpu.get_gyro_range().unwrap(), GyroRange::D250);
+    assert_eq!(mpu.get_sleep_enabled().unwrap(), true);
+    assert_eq!(mpu.get_temp_enabled().unwrap(), true);
+
+    rprintln!("Test successful");
+}
